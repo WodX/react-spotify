f30af3211de94c84cf03ddec4444ea29059ff893
Andre Nunes <a.sousa@runtime-revolution.com>
2021-12-17 14:53:33 +0000

    Fix login issue

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -1,4 +1,3 @@
-
 const endpoint = process.env.REACT_APP_ENDPOINT;
 const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 const clientId = process.env.REACT_APP_CLIENT_ID;
@@ -12,7 +11,7 @@ const scopes = [
   "user-follow-modify",
   "playlist-modify-public",
   "playlist-modify-private",
-  "playlist-read-private"
+  "playlist-read-private",
 ];
 
 export const loginUrl = `${endpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join(
@@ -20,132 +19,196 @@ export const loginUrl = `${endpoint}?client_id=${clientId}&response_type=code&re
 )}`;
 
 const headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Authorization": 'Bearer ' + localStorage.getItem("access_token")
-}
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  Authorization: "Bearer " + localStorage.getItem("access_token"),
+};
 
 const methods = {
   get: {
     method: "GET",
-    headers
+    headers,
   },
   put: {
     method: "PUT",
-    headers
+    headers,
   },
   delete: {
     method: "DELETE",
-    headers
-  }
-}
+    headers,
+  },
+};
 
 export const getToken = (code) => {
-
   return fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
-    headers: { 
+    headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": 'Basic ' + btoa(clientId + ":" + clientSecret)
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
     },
     body: new URLSearchParams({
       code: code,
       grant_type: "authorization_code",
-      redirect_uri: redirectUri
-    })
+      redirect_uri: redirectUri,
+    }),
   });
-}
+};
 
 export const getCurrentUserInfo = async () => {
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
   return fetch("https://api.spotify.com/v1/me", methods.get).then((data) => {
-    return data.json()
+    return data.json();
   });
-}
+};
 
 export const getCurrentUserAlbums = async () => {
-  return fetch("https://api.spotify.com/v1/me/albums?market=PT", methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    "https://api.spotify.com/v1/me/albums?market=PT",
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getAlbum = async (album_id) => {
-  return fetch(`https://api.spotify.com/v1/albums/${album_id}?market=PT`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/albums/${album_id}?market=PT`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getArtistAlbums = async (artist_id) => {
-  return fetch(`https://api.spotify.com/v1/artists/${artist_id}/albums?market=PT`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/artists/${artist_id}/albums?market=PT`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getCurrentUserPlaylists = async () => {
-  return fetch("https://api.spotify.com/v1/me/playlists?market=PT", methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    "https://api.spotify.com/v1/me/playlists?market=PT",
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getPlaylist = async (playlist_id) => {
-  return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}?market=PT`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/playlists/${playlist_id}?market=PT`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getCurrentUserArtists = async () => {
-  return fetch("https://api.spotify.com/v1/me/following?type=artist&limit=20&market=PT", methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    "https://api.spotify.com/v1/me/following?type=artist&limit=20&market=PT",
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getFeaturePlaylists = async () => {
-  return fetch("https://api.spotify.com/v1/browse/featured-playlists?market=PT", methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    "https://api.spotify.com/v1/browse/featured-playlists?market=PT",
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getNewReleases = async () => {
-  return fetch("https://api.spotify.com/v1/browse/new-releases?limit=20&market=PT", methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    "https://api.spotify.com/v1/browse/new-releases?limit=20&market=PT",
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const getArtistTopTracks = async (id) => {
-  return fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks?market=PT`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/artists/${id}/top-tracks?market=PT`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const searchPlaylist = async (q) => {
-  return fetch(`https://api.spotify.com/v1/search?q=${q}&type=playlist,album,artist`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/search?q=${q}&type=playlist,album,artist`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const followPlaylist = async (playlist_id) => {
-  return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/followers`, methods.put);
-}
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/playlists/${playlist_id}/followers`,
+    methods.put
+  );
+};
 
 export const unfollowPlaylist = async (playlist_id) => {
-  return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/followers`, methods.delete);
-}
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/playlists/${playlist_id}/followers`,
+    methods.delete
+  );
+};
 
 export const isFollowingPlaylist = async (user_id, playlist_id) => {
-  return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/followers/contains?ids=${user_id}`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/playlists/${playlist_id}/followers/contains?ids=${user_id}`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
 
 export const followArtist = async (artist_id) => {
-  return fetch(`https://api.spotify.com/v1/me/following?type=artist&ids=${artist_id}`, methods.put);
-}
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/me/following?type=artist&ids=${artist_id}`,
+    methods.put
+  );
+};
 
 export const unfollowArtist = async (artist_id) => {
-  return fetch(`https://api.spotify.com/v1/me/following?type=artist&ids=${artist_id}`, methods.delete);
-}
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/me/following?type=artist&ids=${artist_id}`,
+    methods.delete
+  );
+};
 
 export const isFollowingArtist = async (artist_id) => {
-  return fetch(`https://api.spotify.com/v1/me/following/contains?type=artist&ids=${artist_id}`, methods.get).then((data) => {
-    return data.json()
+  headers.Authorization = "Bearer " + localStorage.getItem("access_token");
+  return fetch(
+    `https://api.spotify.com/v1/me/following/contains?type=artist&ids=${artist_id}`,
+    methods.get
+  ).then((data) => {
+    return data.json();
   });
-}
+};
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,86 +1,127 @@
 import React, { useEffect, useState } from "react";
-import { getCurrentUserInfo, getCurrentUserAlbums, getCurrentUserPlaylists, getFeaturePlaylists, getNewReleases, searchPlaylist, getCurrentUserArtists } from "../api/spotify";
+import {
+  getCurrentUserInfo,
+  getCurrentUserAlbums,
+  getCurrentUserPlaylists,
+  getFeaturePlaylists,
+  getNewReleases,
+  searchPlaylist,
+  getCurrentUserArtists,
+} from "../api/spotify";
 import List from "./List";
 
 const Dashboard = () => {
-	const [user, setUser] = useState({});
-	const [albums, setAlbums] = useState([]);
-	const [playlists, setPlaylists] = useState([]);
-	const [artists, setArtists] = useState([]);
-	const [fPlaylists, setFPlaylists] = useState([]);
-	const [releases, setReleases] = useState([]);
-	const [searchParam, setSearchParam] = useState("");
-	const [searchData, setSearchData] = useState();
+  const [user, setUser] = useState({});
+  const [albums, setAlbums] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [fPlaylists, setFPlaylists] = useState([]);
+  const [releases, setReleases] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
+  const [searchData, setSearchData] = useState();
 
-	useEffect(() => {
-		getCurrentUserInfo().then((info) => {
-			setUser(info);
-		});
+  useEffect(() => {
+    getCurrentUserInfo().then((info) => {
+      setUser(info);
+    });
 
-		getCurrentUserAlbums().then((albums) => {
-			setAlbums(albums.items);
-		});
+    getCurrentUserAlbums().then((albums) => {
+      setAlbums(albums?.items);
+    });
 
-		getCurrentUserPlaylists().then((playlists) => {
-			setPlaylists(playlists.items);
-		});
+    getCurrentUserPlaylists().then((playlists) => {
+      setPlaylists(playlists.items);
+    });
 
-		getCurrentUserArtists().then(({artists}) => {
-			setArtists(artists.items);
-		});
+    getCurrentUserArtists().then(({ artists }) => {
+      setArtists(artists.items);
+    });
 
-		getFeaturePlaylists().then((feature) => {
-			setFPlaylists(feature.playlists.items);
-		});
+    getFeaturePlaylists().then((feature) => {
+      setFPlaylists(feature.playlists.items);
+    });
 
-		getNewReleases().then((inside_release) => {
-			setReleases(inside_release.albums.items);
-		});
+    getNewReleases().then((inside_release) => {
+      setReleases(inside_release.albums.items);
+    });
+  }, []);
 
-	}, []);
-
-
-	return (
-		<div className="container">
-			<h1>User Info</h1>
-			<hr/>
-			{Object.keys(user).length > 0 && (
-				<div className="user-info">
-					<img src={user.images[0]?.url} alt="profile_image" width={200} />
-					<div>
-						<p><b>Name:</b> {user.display_name}</p>
-						<p><b>Email:</b> {user.email}</p>
-						<p><b>Product:</b> {user.product}</p>
-						<p><b>Followers:</b> {user.followers.total}</p>
-						<p><b>Profile:</b> <a href={user.external_urls.spotify}>{user.external_urls.spotify}</a></p>
-					</div>
-				</div>
-			)}
-			<h1>Search</h1>
-			<hr/>
-			<div>
-				<input type="text" onChange={(event) => {setSearchParam(event.target.value)}} placeholder="Search Playlist"/>
-				<button onClick={() => {searchPlaylist(searchParam).then((data) => {setSearchData(data)})}}>Search</button>
-			</div>
-			{searchData && (
-				<div>
-					<p>My search</p>
-					<List title="Playlists" data={searchData.playlists.items} route="playlist" />
-					<List title="Albums" data={searchData.albums.items} route="album" />
-					<List title="Artists" data={searchData.artists.items} route="artist" />
-				</div>
-			)}
-			<h1>Browse</h1>
-			<hr/>
-			<List title="Featured Playlists" data={fPlaylists} route="playlist" />
-			<List title="New Releases" data={releases} route="album" />
-			<h1>My Library</h1>
-			<hr/>
-			<List title="My Albuns" data={albums} route="album" />
-			<List title="My Playlists" data={playlists} route="playlist" />
-			<List title="My Artists" data={artists} route="artist" />
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>User Info</h1>
+      <hr />
+      {Object.keys(user).length > 0 && user.images && (
+        <div className="user-info">
+          <img src={user.images[0]?.url} alt="profile_image" width={200} />
+          <div>
+            <p>
+              <b>Name:</b> {user.display_name}
+            </p>
+            <p>
+              <b>Email:</b> {user.email}
+            </p>
+            <p>
+              <b>Product:</b> {user.product}
+            </p>
+            <p>
+              <b>Followers:</b> {user.followers.total}
+            </p>
+            <p>
+              <b>Profile:</b>{" "}
+              <a href={user.external_urls.spotify}>
+                {user.external_urls.spotify}
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
+      <h1>Search</h1>
+      <hr />
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            setSearchParam(event.target.value);
+          }}
+          placeholder="Search Playlist"
+        />
+        <button
+          onClick={() => {
+            searchPlaylist(searchParam).then((data) => {
+              setSearchData(data);
+            });
+          }}
+        >
+          Search
+        </button>
+      </div>
+      {searchData && (
+        <div>
+          <p>My search</p>
+          <List
+            title="Playlists"
+            data={searchData.playlists.items}
+            route="playlist"
+          />
+          <List title="Albums" data={searchData.albums.items} route="album" />
+          <List
+            title="Artists"
+            data={searchData.artists.items}
+            route="artist"
+          />
+        </div>
+      )}
+      <h1>Browse</h1>
+      <hr />
+      <List title="Featured Playlists" data={fPlaylists} route="playlist" />
+      <List title="New Releases" data={releases} route="album" />
+      <h1>My Library</h1>
+      <hr />
+      <List title="My Albuns" data={albums} route="album" />
+      <List title="My Playlists" data={playlists} route="playlist" />
+      <List title="My Artists" data={artists} route="artist" />
+    </div>
+  );
 };
 
 export default Dashboard;
